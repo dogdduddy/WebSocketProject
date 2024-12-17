eedd1ce22a200b4567a025c33c6491c1fef5def6
dogdduddy <dogdduddy@gmail.com>
2024-12-17 18:21:01 +0900

    Back-End 프로젝트 init

--- WebSOcketProject_aos/app/build.gradle.kts
+++ WebSOcketProject_aos/app/build.gradle.kts
@@ -44,4 +44,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+    // okHttp
+    implementation("com.squareup.okhttp3:okhttp:4.9.3")
 }
